1f58680d53f2f6e21eaddfe40996bc0cebb0c3b5
Agnes <agnes.vj28@gmail.com>
2024-11-05 15:23:26 +0000

    add some new variables

--- CSBasics/CSBasics/Program.cs
+++ CSBasics/CSBasics/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace CSBasics
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CSBasics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double pi = 3.14;
+            bool isLearning = true;
+            string favouriteMovie = "KungFu Ponda1";
+            Console.WriteLine("My Favourite Movie : " + favouriteMovie);
+            favouriteMovie = "High School Musical";
+            Console.WriteLine("My favourite film is " + favouriteMovie);
+
         }
     }
 }
